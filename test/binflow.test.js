26f6cd26434e2f688643d8226a5762d99018d2fe
fediev <fediev@gmail.com>
2016-07-19 15:34:52 +0900

    Add: _hasEndian()

--- test/binflow.test.js
+++ test/binflow.test.js
@@ -1,13 +1,48 @@
+/* eslint-disable no-unused-expressions */
 const chai = require('chai');
 // eslint-disable-next-line no-unused-vars
 const should = chai.should();
 const binflow = require('../lib/binflow');
+
+const tokens = [
+  'double',
+  'float',
+  'int8',
+  'int16',
+  'int32',
+  'uint8',
+  'unit16',
+  'uint32',
+  'char',
+  'string',
+];
+
+const tokensLE = tokens.map((token) => `${token}LE`);
+const tokensBE = tokens.map((token) => `${token}BE`);
+// const tokensAll = [...tokens, ...tokensLE, ...tokensBE];
 
 describe('binflow', () => {
   describe('createBinflow()', () => {
     it('should create a binflow object', () => {
       const result = binflow.createBinflow();
       result.should.be.an('object');
+    });
+  });
+  describe('_hasEndian()', () => {
+    const _hasEndian = binflow.createBinflow()._hasEndian;
+
+    it('should get false on normal tokens', () => {
+      tokens.forEach((token) => {
+        _hasEndian(token).should.be.false;
+      });
+    });
+    it('should get true on endian-tailed tokens', () => {
+      tokensLE.forEach((token) => {
+        _hasEndian(token).should.be.true;
+      });
+      tokensBE.forEach((token) => {
+        _hasEndian(token).should.be.true;
+      });
     });
   });
 });
